cd65c8bb894263ffa6a0c0528e4e9de08de13683
Henrique <henrique.plima1@gmail.com>
2019-09-01 21:25:25 -0300

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCCyNtqMj74isItq8e9okl1ui_QQoMwDik",
+    authDomain: "angularproject-b646b.firebaseapp.com",
+    databaseURL: "https://angularproject-b646b.firebaseio.com",
+    projectId: "angularproject-b646b",
+    storageBucket: "angularproject-b646b.appspot.com",
+    messagingSenderId: "956713631273",
+    appId: "1:956713631273:web:a492e2c9441c6c00"
+  }
 };
